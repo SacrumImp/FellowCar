146c471edcb6a299efaa6893c2df99e39722dd85
VladislavAlpeev <vlad.alpeev@mail.ru>
2022-10-25 00:18:29 +0300

    added Authentication controller

---
+++ FellowCarAPI/FellowCarAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FellowCarAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
